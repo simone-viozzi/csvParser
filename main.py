23f163de0d97afef0ff4b1db7999a57770d07f0a
simone-viozzi <viozzis333@gmail.com>
2019-07-08 22:43:29 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import re
+
+
+def read():
+    f = open("eu_results.csv", "rb")
+    csv = f.read()
+    return csv
+
+
+if __name__ == '__main__':
+    csv = read()
+    i = 28
+    
+    for s in csv.split(b'\r\n'):
+        arr = re.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", s.decode('utf-8'))
+        print(arr)
